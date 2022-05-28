e2d0de8cab451b469e12ecf29abc7e48f09eca39
Erick Chávez <ericksebastianchavezgarcia4gmail.com>
2022-05-27 20:18:38 -0500

    Segundo Cambio

--- src/main/java/com/uce/edu/demo/consultorio/Paciente.java
+++ src/main/java/com/uce/edu/demo/consultorio/Paciente.java
@@ -1,5 +1,29 @@
 package com.uce.edu.demo.consultorio;
 
 public class Paciente {
+	private String nombre;
+	private Integer edad;
 
+	
+	@Override
+	public String toString() {
+		return "Paciente [nombre=" + nombre + ", edad=" + edad + "]";
+	}
+
+	//SET Y GET
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getEdad() {
+		return edad;
+	}
+
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
 }
